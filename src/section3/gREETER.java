f7708ed158e378a1df4e2ba02642ff6fdcee6f94
League Student <league@iMac-20.attlocal.net>
2020-02-08 15:49:58 -0800

    section 3 in progress
    yeet

---
+++ src/section3/gREETER.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class gREETER {
+	public static void main(String[] args) {
+		System.out.println("Esteban");
+		String Name = JOptionPane.showInputDialog("what is your name");
+		JOptionPane.showMessageDialog(null,Name);
+		System.out.println(Name + " will be the best coder");
+		
+	}
+}
