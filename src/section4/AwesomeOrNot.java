9d3708d7a3e6e51690f23422abece74cf4630056
League Student <league@iMac-94.attlocal.net>
2020-02-15 15:53:26 -0800

    finished part 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,12 +2,32 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+	public static void main(String[] args) {
+		Random yeet = new Random ();
+		int randomNumber = yeet.nextInt(4);
+		String awesome = JOptionPane.showInputDialog("what is something you think is awesome");
+		if(randomNumber==0) {
+			JOptionPane.showMessageDialog(null, "i think that" + awesome + "is really cool also");
+		}
+		if(randomNumber==1) {
+			JOptionPane.showMessageDialog(null, "eh," + awesome + "is ok i guess,");
+		}
+		if(randomNumber==2) {
+			JOptionPane.showMessageDialog(null, "oh, i think that" + awesome + "is really boring");
+		}
+		if(randomNumber==3) {
+			JOptionPane.showMessageDialog(null, "omg u flippin normie,"+ awesome + "is not memelord") ;
+		}
+		
+		
+	}
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	
 	// 3. Print out this variable
 
 	// 4. Get the user to enter something that they think is awesome
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,38 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
-		
-		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer was correct, add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		// 2. Ask the user a question
+		String Q1 = JOptionPane
+				.showInputDialog("is the amazon river in north america, south america, or in the pacific ocean?");
+		String Q2 = JOptionPane.showInputDialog("is lazar beam the yeetlord?");
+		String Q3 = JOptionPane.showInputDialog("is kobe (RIP IN Peace) helicopter pilot an airplane pilot");
+		String Q4 = JOptionPane.showInputDialog("is er1C big meme lord");
+		// 3. Use an if statement to check if their answer is correct
+		if (Q1.equalsIgnoreCase("south america")) {
+			score++;
+		}
+		if (Q2.equalsIgnoreCase("yes")) {
+			score++;
+		}
+		if (Q3.equalsIgnoreCase("no")) {
+			score++;
+		}
+		if (Q4.equalsIgnoreCase("yes")) {
+			score += 1000;
+		}
+
+		// 4. if the user's answer was correct, add one to their score
+
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+
+		// 6. After all the questions have been asked, print the user's score
+		JOptionPane.showMessageDialog(null, "bruh, u r no stonks, your score is ," + score);
 	}
 }
